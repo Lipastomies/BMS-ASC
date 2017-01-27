578c18846ecb78e572cf243da353833e87e6ced2
Lipastomies <Lipastomies@users.noreply.github.com>
2017-01-27 22:51:38 +0200

    Added the actual arduino code

---
+++ bms.ino
@@ -0,0 +1,195 @@
+#include "HID-Project.h"
+
+void doEncoderA();
+void doEncoderB();
+
+// button layout
+// Button     -- Pin # --  Keyboard -- led
+// Select       A0          Space        2
+// Start        A1          Enter        3
+// BT1          A2          a            4
+// BT2          A3          s            5
+// BT3          A4          d            6
+// BT4          A5          f            7
+// BT5          11          g            8
+// BT6          12          h            9
+// BT7          13          j            10
+//scratch left  --          u            --
+//scratch right --          i            --
+
+// ENCODERS   DATA 1  DATA 2
+// Encoder     0      1
+//
+
+#define DELAY 2  // Delay per loop in ms
+ enum PinAssignments
+ {
+  encoderPinA = 0,
+  encoderPinB = 1
+ };
+//This is up to encoder pin wiring
+
+int keypins[] = {A0,A1,A2,A3,A4,A5,11,12,13};
+int ledpins[] = {2,3,4,5,6,7,8,9,10};
+char keyvalues[] = {' ','\n','a','s','d','f','g','h','j'};
+char scratchvalues[] = {'u','i'};
+
+int encoderPos = 0;
+static boolean rotating = false;
+ 
+boolean A_set = false;              
+boolean B_set = false;
+ 
+//scratch parameters, change these to change scratch functionality
+const int scratch_sense = 8;//how many steps the encoder has to move for scratch to register
+const int  scratchdelay = 10;//how long the scratch is on after activation. Important for continuous rotation of scratch.
+const int stopdelay = 5;//how long the encoder has to change its position by scratch sense, otherwise scratch will not register.
+//scratch variables
+int tempposition = 0;
+int sc_left,sc_right=0;
+int temp_direction=0;
+int delaycounter=0;
+int stopcounter=0;
+
+void setup() {
+  //find out encoder pin interrupt numbers
+  int encoder0 = digitalPinToInterrupt(encoderPinA);
+  int encoder1 = digitalPinToInterrupt(encoderPinB);
+
+  for (int i= 0;i<9;i++)
+    pinMode(keypins[i], INPUT_PULLUP);
+
+  NKROKeyboard.begin();
+  for (int i=0;i<9;i++)
+    pinMode(ledpins[i], OUTPUT);
+  pinMode(encoderPinA, INPUT_PULLUP);
+  pinMode(encoderPinB, INPUT_PULLUP);
+ 
+  attachInterrupt(encoder0, doEncoderA, CHANGE);
+  attachInterrupt(encoder1, doEncoderB, CHANGE);
+  //serial for debugging e.g. the scratch
+  //Serial.begin(9600);
+}
+
+void loop() {
+  //read the keys and communicate accordingly, as well as set the leds
+  for (int i=0;i<9;i++){
+    if(digitalRead(keypins[i])==LOW){
+    NKROKeyboard.press(keyvalues[i]);
+    digitalWrite(ledpins[i],HIGH);
+    }
+    if(digitalRead(keypins[i])==HIGH){
+      NKROKeyboard.release(keyvalues[i]);
+      digitalWrite(ledpins[i],LOW);
+    }
+  }
+  //Encoder Reset
+  rotating = true;
+  if (encoderPos != 0)
+  {
+    tempposition += encoderPos;
+    encoderPos = 0;
+  }
+
+  //scratch registration
+  delaycounter++;
+  if (tempposition > scratch_sense){
+    temp_direction=1;
+    tempposition=0;  
+    delaycounter=0;
+  }
+  else if (tempposition < -scratch_sense){
+    temp_direction=-1;
+    tempposition=0;
+    delaycounter=0;
+  }
+  else temp_direction=0;
+  //nollasta oikealle
+
+  //scratch debouncing
+  if (stopcounter > stopdelay ){
+    tempposition=0;
+    temp_direction=0;
+    stopcounter=0;
+  }
+  if (temp_direction == 0)
+    stopcounter++;
+    
+  //scratch communication to the computer
+  //scratching from zero to right
+  if (sc_right == 0 && temp_direction == 1){
+    NKROKeyboard.release(scratchvalues[0]);
+    NKROKeyboard.press(scratchvalues[1]);
+    sc_right =1;
+  }
+  //scratching from zero to left
+  else if (sc_left==0 && temp_direction == -1){
+    NKROKeyboard.release(scratchvalues[1]);
+    NKROKeyboard.press(scratchvalues[0]);
+    sc_left =1;
+  }
+  //scratching from left to right
+  else if (sc_left==1 && temp_direction == 1){
+    NKROKeyboard.release(scratchvalues[0]);
+    NKROKeyboard.press(scratchvalues[1]);
+    sc_left=0;
+    sc_right=1;
+  }
+  //scratching from right to left
+  else if (sc_right==1 && temp_direction == -1){
+    NKROKeyboard.release(scratchvalues[1]);
+    NKROKeyboard.press(scratchvalues[0]);
+    sc_left=1;
+    sc_right=0;
+  }
+  //scratch deactivation
+  
+  //scratch weas moving to left, but has stopped/is stopping
+  else if (sc_left==1 && temp_direction==0 && delaycounter > scratchdelay){
+    NKROKeyboard.release(scratchvalues[0]);
+    sc_left=0;
+    delaycounter= 0;
+  }
+  //the same, but for right
+  else if (sc_right==1 && temp_direction==0 && delaycounter > scratchdelay){
+    NKROKeyboard.release(scratchvalues[1]);
+    sc_right=0;
+    delaycounter=0;
+  }
+  
+  delay(DELAY);
+
+  //report of scratch for debugging. Good for adjusting scratch parameters.
+  /* 
+  Serial.print(sc_left);
+  Serial.print(' ');
+  Serial.print(sc_right);
+  Serial.print(' ');
+  Serial.print(temp_direction);
+  Serial.print(' ');
+  Serial.print(tempposition);
+  Serial.print('\n');  
+  */
+}
+
+void doEncoderA()
+{
+  if( digitalRead(encoderPinA) != A_set )
+  {  
+    A_set = !A_set;
+    if ( A_set && !B_set )
+      encoderPos += 1;
+    rotating = false;  
+  }
+}
+ 
+void doEncoderB()
+{
+ 
+  if( digitalRead(encoderPinB) != B_set ) {
+    B_set = !B_set;
+    if( B_set && !A_set )
+      encoderPos -= 1;
+    rotating = false;
+  }
+}
